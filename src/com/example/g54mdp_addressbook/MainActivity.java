304ac0b90fcaeaddd9226e8f7c10dc0341bd8841
Tai <psytn2@nottingham.ac.uk>
2014-03-25 10:50:04 +0000

    add contacts activity added to main activity

--- src/com/example/g54mdp_addressbook/MainActivity.java
+++ src/com/example/g54mdp_addressbook/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.g54mdp_addressbook;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void addContact(View view) {
+		Intent addContactIntent = new Intent(getApplicationContext(), AddContactActivity.class);
+		startActivity(addContactIntent);
 	}
 
 }
